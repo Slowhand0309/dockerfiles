0462b68e35d256825c98df46b9f3074907488bb6
Slowhand0309 <slowhand0309@gmail.com>
2016-11-29 01:02:23 +0900

    Update rails new container

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,14 @@ ENV HOME_DIR /usr/src/app
 WORKDIR $HOME_DIR
 
 RUN apt-get update -qq
+RUN apt-get install -y sqlite3
+RUN apt-get install -y nodejs
 
-ADD Gemfile $HOME_DIR/Gemfile
-ADD Gemfile.lock $HOME_DIR/Gemfile.lock
+COPY Gemfile $HOME_DIR/
+COPY Gemfile.lock $HOME_DIR/
+
 RUN bundle install
 ADD . $HOME_DIR
+
+# Publish port 3000
+EXPOSE 3000
--- rails_new.sh
+++ rails_new.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -e Gemfile ]; then
-  rm Gemfile
-fi
-if [ -e Gemfile.lock ]; then
-  rm Gemfile.lock
-fi
-echo 'source "https://rubygems.org"' >> Gemfile
-echo 'gem "rails"' >> Gemfile
-touch Gemfile.lock
+docker-compose run --rm web rails new .
+docker-compose run --rm web rake db:setup
 
-docker-compose run web rails new . --skip-bundle
