912b0e7144cb467943804e4e6f52b2d8bd563978
Slowhand0309 <slowhand0309@gmail.com>
2016-12-04 18:37:01 +0900

    Use CircleCI

---
+++ run-tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+WORKDIR=$PED
+
+# Find target directory.
+for entity in `find . -type d -mindepth 1 -maxdepth 1 -not -name ".git"`; do
+  # Only exist spec directory.
+  if [ -e $entity/spec ]; then
+    # Build docker image.
+    cd $entity
+    docker-compose build
+    cd spec
+    # Install gems via bundler.
+    bundle install --path vendor/bundle
+    bundle exec rspec
+    cd $WORKDIR
+  fi
+done
