469c6e9853abbdb61a0d8393a760b3735cbae37e
Slowhand0309 <slowhand0309@gmail.com>
2016-12-25 00:39:21 +0900

    Add spec for 'gcc_cmake' docker image

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
+# Rails
 docker push slowhand/rails:1.0
+
+# gcc + cmake
+docker push slowhand/gcc_cmake:1.0
---
+++ gcc_cmake/spec/gcc_cmake_docker_spec.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+require 'spec_helper'
+
+set :docker_image, 'slowhand/gcc_cmake:1.0'
+
+if ENV['CIRCLECI']
+  class Docker::Container
+    def remove(options={}); end
+    alias_method :delete, :remove
+  end
+end
+
+# Check for should be installed cmake.
+describe command('cmake --version') do
+  its(:stdout) { should match /^cmake version 3.7.1/ }
+end
+
+# Check for should be installed gtest.
+describe file('/usr/local/include/gtest') do
+  it { should be_directory }
+end
+
+describe file('/usr/local/include/gtest/gtest.h') do
+  it { should exist }
+end
+
+%w(libgtest.a libgtest_main.a).each do |lib|
+  describe file(lib) do
+    it { should exist }
+  end
+end
