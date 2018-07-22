c36aac7b39899ce9eefe8aca96eb28bb25ccd01b
Slowhand0309 <slowhand0309@gmail.com>
2018-07-23 01:14:55 +0900

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-sudo docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
 # Android
-sudo docker push slowhand/android:1.0
+docker push slowhand/android:1.0
 
 # Rails
-sudo docker push slowhand/rails:1.0
+docker push slowhand/rails:1.0
 
 # gcc + cmake
-sudo docker push slowhand/gcc_cmake:1.0
+docker push slowhand/gcc_cmake:1.0
